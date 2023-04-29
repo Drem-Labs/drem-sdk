bd8886f7f0c4eaaf10f6eb7854813f92ff7423b6
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2023-04-29 12:17:48 -0400

    made sdk base

--- config.ts
+++ config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from '@dethcrypto/eth-sdk'
 
+
+// to make things more efficient, eth-sdk uses the abis in the abis/ folder
+  // since many of these are proxies, I recommend adding the abis yourself
 export default defineConfig({
   contracts: {
     polygonMumbai: {
-      dai: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      StepTreeLib: "0x475C7FACa98F18588C0281185bC35389dA90B5FA",
+      DremHub: "0x27f04bAE5C17f8556D58c3c6B32F030643C47F5a",
+      FeeController: "0x9f814Ff3fE04099cDD04Ad93C0307fE09F8194f9",
+      PriceAggreagtor: "0xd804c3DF1AB160fc7cb406F9902ea3be43107e42",
+      AssetRegistry: "0x6dC1CD636fd04C437F48d61d92C4CC2AccBAC1cB",
+      VaultDeployer: "0x33334081Bb86EDDDa17ac9c90E347Dd0ec2EDed9"
     },
   },
 })
---
+++ createSDK.sh
@@ -0,0 +1,7 @@
+# create a blank config file
+
+# copy the abi file to the config
+
+# run eth-sdk
+
+
