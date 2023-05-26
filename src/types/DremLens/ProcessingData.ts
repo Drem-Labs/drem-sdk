81c1ba36cf5b37d44ee703451603d81c9332da7d
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2023-05-26 11:42:01 -0400

    processing data struct prototype

--- src/types/DremLens/ProcessingData.ts
+++ src/types/DremLens/ProcessingData.ts
@@ -1,3 +1,24 @@
+import { StepTree } from '../../stepTree';
+
+export type ProcessingDataStruct = [number, string[]];
+
 export class ProcessingData {
-    //
+    // internal data
+    followerTokenId: number;
+    stepTree: StepTree;
+
+    //  constructor to create the processing data
+    constructor(followerTokenId: number, stepTree: StepTree) {
+        // set all the internal data members
+        this.followerTokenId = followerTokenId;  // assuming someone doesn't do something silly, like passing a floating point number
+        this.stepTree = stepTree;
+    }
+
+    // send it to a struct
+    async toStruct(): Promise<ProcessingDataStruct> {
+        return [
+            this.followerTokenId,
+            (await this.stepTree.toVariableArgs())
+            ];
+    }
 }
